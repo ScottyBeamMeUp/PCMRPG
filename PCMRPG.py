5a6049f340e534546596805d546a6ef75eb66108
Andrew Donshik <andrewdonshik@gmail.com>
2015-02-23 12:08:06 -0500

    reorganized init

--- PCMRPG.py
+++ PCMRPG.py
@@ -1,14 +1,8 @@
 import os
-import modules.menus
 cwd = os.getcwd
+## check for all modules and then exec
 if os.path.exists('modules/fight.py') and os.path.exists('modules/store.py') and os.path.exists('modules/menus.py') and os.path.exists('modules/story.py') and os.path.exists('modules/vars.py'):
+	import modules.menus
 	modules.menus.mainmenu()
 else:
 	raise Exception(MissingFile)
-## exec--hold my mouse I'm going in!
-
-## quest 0 is game start
-## quest 1 is reddit post
-## quest 1.1 is positive post
-## quest 1.2 is negative post
-## quest 2 is pending
