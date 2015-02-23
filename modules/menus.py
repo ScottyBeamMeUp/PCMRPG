3e21563d3b694ee7ece7f3eecbbd14af8689b2e3
Andrew Donshik <andrewdonshik@gmail.com>
2015-02-23 00:28:15 -0500

    fixed invocation again

--- modules/menus.py
+++ modules/menus.py
@@ -39,7 +39,7 @@ def gamemenu(): ## main menu once in game
 		elif choice == "i":
 			inventory()
 		elif choice == "f":
-			fight.genfight(blank)
+			fight.genfight(vars.blank)
 def loadfile(): ## for loading the pickle dump- must be saved as pcmrpgsave.txt in same folder
 	vars.stats
 	with open('pcmrpgsave.txt', 'rb') as handle:
