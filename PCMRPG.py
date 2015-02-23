520e45987483fce03d9ef0c9ddb92c6a11f5a21c
Andrew Donshik <andrewdonshik@gmail.com>
2015-02-23 12:08:33 -0500

    removed unnecessary variable

--- PCMRPG.py
+++ PCMRPG.py
@@ -1,5 +1,4 @@
 import os
-cwd = os.getcwd
 ## check for all modules and then exec
 if os.path.exists('modules/fight.py') and os.path.exists('modules/store.py') and os.path.exists('modules/menus.py') and os.path.exists('modules/story.py') and os.path.exists('modules/vars.py'):
 	import modules.menus
