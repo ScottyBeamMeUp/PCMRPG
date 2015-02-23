d3fd9cd1d8ee1b5e937bf368eab46882a211ffb0
Andrew Donshik <andrewdonshik@gmail.com>
2015-02-23 11:49:29 -0500

    Added module check

--- PCMRPG.py
+++ PCMRPG.py
@@ -1,6 +1,12 @@
+import os
 import modules.menus
+cwd = os.getcwd
+if os.path.exists('modules/fight.py') and os.path.exists('modules/store.py') and os.path.exists('modules/menus.py') and os.path.exists('modules/story.py') and os.path.exists('modules/vars.py'):
+	modules.menus.mainmenu()
+else:
+	raise Exception(MissingFile)
 ## exec--hold my mouse I'm going in!
-modules.menus.mainmenu()
+
 ## quest 0 is game start
 ## quest 1 is reddit post
 ## quest 1.1 is positive post
