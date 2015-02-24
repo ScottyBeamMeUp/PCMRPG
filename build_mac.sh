24a836495b73a7ccae6818fed739431b87460902
Andrew Donshik <andrewdonshik@gmail.com>
2015-02-24 14:01:07 -0500

    changed case in build_mac

--- build_mac.sh
+++ build_mac.sh
@@ -3,7 +3,7 @@ if which pyinstaller >/dev/null; then
   pyinstaller --onefile PCMRPG.py
   cp dist/PCMRPG PCMRPGshell.app/Contents/Resources/Scripts/
 else
-  echo Pyinstaller is not installed. Please install it.
+  echo pyinstaller is not installed. Please install it.
   while true; do
     read -p "Do you wish to resolve dependencies?" yn
     case $yn in
