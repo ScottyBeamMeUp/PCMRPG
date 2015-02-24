4188221e589fe601ab0751ebe6295e510ec0795f
Andrew Donshik <andrewdonshik@gmail.com>
2015-02-24 10:33:40 -0500

    quick fix

--- modules/vars.py
+++ modules/vars.py
@@ -60,4 +60,5 @@ storefront3nonfree = """
 		To heal yourself, type repair.
 		"""
 afteryoutube == """
-The maker of the video thanks you and recommends that you buy yourself a CPU."
+The maker of the video thanks you and recommends that you buy yourself a CPU.
+"""
