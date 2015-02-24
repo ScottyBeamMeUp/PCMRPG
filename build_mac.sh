c9cbfeaf2c284e8355d64d54a483d242f9c7a0d9
Andrew Donshik <andrew@donshikfamily.com>
2015-02-24 13:14:18 -0500

    Update build_mac.sh
    Sanity check

--- build_mac.sh
+++ build_mac.sh
@@ -1,2 +1,7 @@
-pyinstaller --onefile PCMRPG.py
-cp dist/PCMRPG PCMRPGshell.app/Contents/Resources/Scripts/
+#!/bin/bash
+if which pyinstaller >/dev/null; then
+  pyinstaller --onefile PCMRPG.py
+  cp dist/PCMRPG PCMRPGshell.app/Contents/Resources/Scripts/
+else
+  echo Pyinstaller is not installed. Please install it.
+fi
